8be046fc9bcf29245f0baca411ff67661d4b51a5
Tom Hoen <thoen@classroomparent.com>
2024-10-29 12:49:00 -0400

    update the form_ui method
    We need to ensure that we are not trying to display a file field url when the record has not been persisted, as usually that url will contain the id of the record, which will be blank

--- lib/active_scaffold/bridges/paperclip/form_ui.rb
+++ lib/active_scaffold/bridges/paperclip/form_ui.rb
@@ -4,7 +4,10 @@ module ActiveScaffold
       def active_scaffold_input_paperclip(column, options, ui_options: column.options)
         record = options[:object]
         paperclip = record.send(column.name.to_s)
-        content = active_scaffold_column_paperclip(record, column, ui_options: ui_options) if paperclip.file?
+        # TDH: 2024-10-29 so that a non-persisted record can show the file upload field we need to check if the record is persisted
+        # if we attempt to show the column when the record is not persisted, it will attempt to find the file with a blank id. 
+        # this causes an error and the form will not render properly. This is the only change to the original code.
+        content = active_scaffold_column_paperclip(record, column, ui_options: ui_options) if paperclip.file? && record.persisted?
         active_scaffold_file_with_remove_link(column, options, content, 'delete_', 'paperclip_controls', ui_options: ui_options)
       end
     end
